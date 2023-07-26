5dd85f19b9d2408bdefadf6c8eaaa4ec25e5b41e
lazydev01 <arpeetbhavsar01@gmail.com>
2023-07-26 22:16:03 +0530

    Added SecurityFilterChain, InMemoryAuthentication and Role Based Access to the fake Controllers

--- src/main/java/com/springsecurity/learn/config/MySecurityConfig.java
+++ src/main/java/com/springsecurity/learn/config/MySecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,9 +19,30 @@ public class MySecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
+                        .requestMatchers("/home/**").hasRole("USER")                        .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password(this.passwordEncoder().encode("arpit"))
+                .roles("USER")
+                .build();
+        UserDetails user2 = User.withUsername("user2")
+                .password(this.passwordEncoder().encode("arpit"))
+                .roles("ADMIN")
+                .build();
+        UserDetails user3 = User.withUsername("user3")
+                .password(this.passwordEncoder().encode("arpit"))
+                .roles("EMPLOYEE")
+                .build();
+        return new InMemoryUserDetailsManager(user, user2, user3);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
     }
 }
---
+++ src/main/java/com/springsecurity/learn/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.springsecurity.learn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+    @GetMapping("")
+    public String home(){
+        return "This is Home Page";
+    }
+    @GetMapping("/register")
+    public String register(){
+        return "This is Register Page";
+    }
+    @GetMapping("/login")
+    public String login(){
+        return "This is Login Page";
+    }
+}
